37216796dcd4fa231b87c208af9a71ab9b7074c5
cskiller24 <ccadayong2020@plm.edu.ph>
2022-03-09 13:04:32 +0800

    Added product template

---
+++ frontend/src/components/ProductModal.js
@@ -0,0 +1,95 @@
+import { Button, Input, Modal, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
+import React from "react";
+
+const ProductModal = ({ open, toggleOpen }) => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "75vw",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      name: data.get("name"),
+      price: data.get("price"),
+      image: data.get("image"),
+    });
+    toggleOpen();
+  };
+
+  return (
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h4" component="h2">
+            Add Product
+          </Typography>
+          <CloseIcon onClick={toggleOpen} sx={{ cursor: "pointer" }} />
+        </Box>
+        <Box component="form" onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="Product Name"
+            name="name"
+            sx={{ mt: 3 }}
+          />
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            label="Price"
+            name="price"
+            sx={{ mt: 3 }}
+            type="tel"
+          />
+          <label htmlFor="contained-button-file">
+            <Input
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+              name="image"
+              sx={{ display: "none" }}
+            />
+            <Button
+              variant="outlined"
+              component="span"
+              sx={{ mt: 3 }}
+              fullWidth
+            >
+              Image
+            </Button>
+          </label>
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+            Add Product
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default ProductModal;
--- frontend/src/views/admin/Products.js
+++ frontend/src/views/admin/Products.js
@@ -1,7 +1,41 @@
-import React from "react";
+import { Box, Button, Paper } from "@mui/material";
+import React, { useState } from "react";
+import ProductModal from "../../components/ProductModal";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Button
+        variant="contained"
+        sx={{
+          display: "block",
+          width: "100%",
+          backgroundColor: "secondary",
+          mb: 4,
+        }}
+        onClick={() => setOpen(true)}
+      >
+        Add Product
+      </Button>
+      <ProductModal open={open} toggleOpen={() => setOpen(!open)} />
+      <Box
+        sx={{
+          display: "flex",
+          flexFlow: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Paper>TEST</Paper>
+        <Paper>TEST</Paper>
+        <Paper>TEST</Paper>
+        <Paper>TEST</Paper>
+        <Paper>TEST</Paper>
+        <Paper>TEST</Paper>
+      </Box>
+    </>
+  );
 };
 
 export default Products;
