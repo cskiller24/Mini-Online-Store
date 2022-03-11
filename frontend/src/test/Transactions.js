f6c86fe61640710d4dd9a242f0ec74b9bbaa311d
cskiller24 <ccadayong2020@plm.edu.ph>
2022-03-11 15:40:40 +0800

    added transaction test data

---
+++ frontend/src/test/Transactions.js
@@ -0,0 +1,92 @@
+export const Transactions = [
+  {
+    id: 1,
+    status: "Pending",
+    products: [
+      {
+        id: 1,
+        name: "This is a name",
+        quantity: 4,
+        price: 1000,
+      },
+      {
+        id: 2,
+        name: "This is a name 2",
+        quantity: 6,
+        price: 1000,
+      },
+      {
+        id: 3,
+        name: "This is a name 3",
+        quantity: 5,
+        price: 1000,
+      },
+    ],
+  },
+  {
+    id: 2,
+    status: "Pending",
+    products: [
+      {
+        id: 1,
+        name: "This is a name",
+        quantity: 4,
+        price: 1000,
+      },
+      {
+        id: 2,
+        name: "This is a name 2",
+        quantity: 6,
+        price: 1000,
+      },
+    ],
+  },
+  {
+    id: 3,
+    status: "Pending",
+    products: [
+      {
+        id: 1,
+        name: "This is a name",
+        quantity: 4,
+        price: 1000,
+      },
+    ],
+  },
+  {
+    id: 4,
+    status: "Pending",
+    products: [
+      {
+        id: 1,
+        name: "This is a name",
+        quantity: 4,
+        price: 1000,
+      },
+      {
+        id: 2,
+        name: "This is a name 2",
+        quantity: 6,
+        price: 1000,
+      },
+      {
+        id: 3,
+        name: "This is a name 3",
+        quantity: 5,
+        price: 1000,
+      },
+      {
+        id: 4,
+        name: "This is a name 4",
+        quantity: 5,
+        price: 1000,
+      },
+      {
+        id: 5,
+        name: "This is a name 5",
+        quantity: 5,
+        price: 1000,
+      },
+    ],
+  },
+];
