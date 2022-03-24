91c1a1bc12bde018bc17ca9b7bc5b0b95b1756b1
cskiller24 <ccadayong2020@plm.edu.ph>
2022-03-24 18:49:14 +0800

    added transactions controller

--- app/Http/Controllers/TransactionsController.php
+++ app/Http/Controllers/TransactionsController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\temp\TransactionsResource;
+use App\Models\Transactions;
 
 class TransactionsController extends Controller
 {
-    //
+    const PENDING_STATUS = 0;
+    const SHIPPED_STATUS = 1;
+    const DELIVERED_STATUS = 2;
+    const CANCELLED_STATUS = 3;
+
+    public function index()
+    {
+        $transactions = Transactions::with('product')->paginate(2);
+        return response()->json([
+            'transactions' => TransactionsResource::collection($transactions)
+        ]);
+    }
+
+//    public function show()
+//    {
+//        $transaction = Transactions::where('user_id', auth()->user()->id)->with([
+//            'transaction' => fn($q) => $q->with('product')])->get();
+//        return response()->json([
+//            'user' => $transaction
+//        ]);
+//    }
 }
